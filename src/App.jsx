95440f3819c58ef9021cb1b638a9865b3d1d7597
JossAbreu <josue.abreu.0712@hotmail.com>
2023-08-15 16:12:54 -0400

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Calculator from './components/Calculator'
+
+import Calculator from './components/calculator'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-
-     <Calculator/>
+     <Calculator />
   )
 }
 
